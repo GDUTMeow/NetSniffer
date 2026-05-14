a9eab11bb6e4fd2ef03402b40dff8890ba822750
GamerNoTitle <GamerNoTitle@outlook.com>
2026-05-14 20:40:02 +0800

    feat: 添加初始化监听器部分

--- listener/__init__.py
+++ listener/__init__.py
@@ -1,8 +1,63 @@
 import os
 import socket
-
+import platform
 import network
 
+from exception import SetupRequiredError, NotFoundError
+
+
 class Listener:
-    def __init__(self, host: str = network.get_local_ip(), port: int = 0):
-        ...
+    def __init__(self, interface: str):
+        self.interface = interface
+        self.sniffer = None
+        self.sniffer_v6 = None
+        self.mix_mode = False
+        self.protocol = None
+        self.is_setup = False
+        self.is_running = False
+
+    def setup(self):
+        # Create a raw socket and bind it to the interface
+        if platform.system() == "Linux":
+            # Linux has AF_PACKET which can capture both IPv4 and IPv6
+            self.sniffer = socket.socket(
+                socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003)
+            )
+            self.mix_mode = True
+        elif platform.system() == "Darwin":
+            # MacOS need to set 2 sockets for IPv4 and IPv6
+            self.sniffer = socket.socket(
+                socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP
+            )
+            try:
+                network.get_local_ip(ipv4=False, ipv6=True)  # type: ignore
+                self.sniffer_v6 = socket.socket(
+                    socket.AF_INET6, socket.SOCK_RAW, socket.IPPROTO_IPV6
+                )
+            except NotFoundError:
+                print("[!] No ipv6 address found, skipping ipv6 sniffer setup.")
+        else:
+            # Windows need to turn on ioctl mode
+            self.sniffer = socket.socket(
+                socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP
+            )
+            self.sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+            try:
+                network.get_local_ip(ipv4=False, ipv6=True)  # type: ignore
+                self.sniffer_v6 = socket.socket(
+                    socket.AF_INET6, socket.SOCK_RAW, socket.IPPROTO_IPV6
+                )
+            except NotFoundError:
+                print("[!] No ipv6 address found, skipping ipv6 sniffer setup.")
+        # Bind sockets to interface
+        if self.mix_mode:
+            self.sniffer.bind((self.interface, 0))
+        else:
+            self.sniffer.bind((network.get_local_ip(interface=self.interface), 0))  # type: ignore
+            if self.sniffer_v6:
+                self.sniffer_v6.bind((network.get_local_ip(ipv4=False, interface=self.interface), 0))  # type: ignore
+        self.is_setup = True
+
+    def start(self):
+        if not self.is_setup:
+            raise SetupRequiredError("Listener must be set up before starting.")
--- network/__init__.py
+++ network/__init__.py
@@ -27,22 +27,35 @@ class Local:
 
 
 def get_local_ip(
-    ipv4: bool = True, ipv6: bool = False, prefix: str | None = None
+    ipv4: bool = True, ipv6: bool = False, prefix: str | None = None, interface: str | None = None
 ) -> str:
     if not ipv4 and not ipv6:
         raise OptionError("Neither IPv4 nor IPv6 is chosen.")
-    if ipv4:
-        for _, _, _, _, sockaddr in Local.ipv4_list:
-            if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
-                return str(sockaddr[0])
-    if ipv6:
-        for _, _, _, _, sockaddr in Local.ipv6_list:
-            if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
-                return str(sockaddr[0])
-    raise NotFoundError(f"No matching local IP address matching prefix={prefix} found.")
+    if prefix is not None and interface is not None:
+        raise OptionError("Cannot specify both prefix and interface.")
+    if not interface:
+        if ipv4:
+            for _, _, _, _, sockaddr in Local.ipv4_list:
+                if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
+                    return str(sockaddr[0])
+        if ipv6:
+            for _, _, _, _, sockaddr in Local.ipv6_list:
+                if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
+                    return str(sockaddr[0])
+    else:
+        # Use the interface name specified to get the IP address
+        if ipv4:
+            for _, _, _, canonname, sockaddr in Local.ipv4_list:
+                if canonname == interface:
+                    return str(sockaddr[0])
+        if ipv6:
+            for _, _, _, canonname, sockaddr in Local.ipv6_list:
+                if canonname == interface:
+                    return str(sockaddr[0])
+    raise NotFoundError(f"No matching local IP address matching prefix={prefix}, interface={interface} found.")
 
 def get_all_local_ip(
-    ipv4: bool = True, ipv6: bool = False, prefix: str | None = None
+    ipv4: bool = True, ipv6: bool = False, prefix: str | None = None, interface: str | None = None
 ) -> list[str]:
     if not ipv4 and not ipv6:
         raise OptionError("Neither IPv4 nor IPv6 is chosen.")
