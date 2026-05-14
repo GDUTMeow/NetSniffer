7aa26fbd3308d36dc8d0f0608629c26ff304af5b
GamerNoTitle <GamerNoTitle@outlook.com>
2026-05-14 20:54:18 +0800

    feat: 更改获取 IP 地址的方式，引入 netifaces 库

---
+++ app.py
@@ -0,0 +1,5 @@
+from listener import Listener
+
+listener = Listener("en0")
+listener.setup()
+listener.start()
--- network/__init__.py
+++ network/__init__.py
@@ -1,5 +1,6 @@
 import socket
 from socket import AddressFamily, SocketKind
+from netifaces import interfaces, ifaddresses, AF_INET, AF_INET6
 
 from exception import OptionError, NotFoundError
 
@@ -14,7 +15,9 @@ class Local:
             str,
             tuple[str, int] | tuple[str, int, int, int] | tuple[int, bytes],
         ]
-    ] = socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET, socket.SOCK_STREAM)
+    ] = socket.getaddrinfo(
+        socket.gethostname(), None, socket.AF_INET, socket.SOCK_STREAM
+    )
     ipv6_list: list[
         tuple[
             AddressFamily,
@@ -23,49 +26,34 @@ class Local:
             str,
             tuple[str, int] | tuple[str, int, int, int] | tuple[int, bytes],
         ]
-    ] = socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6, socket.SOCK_STREAM)
+    ] = socket.getaddrinfo(
+        socket.gethostname(), None, socket.AF_INET6, socket.SOCK_STREAM
+    )
 
 
-def get_local_ip(
-    ipv4: bool = True, ipv6: bool = False, prefix: str | None = None, interface: str | None = None
-) -> str:
-    if not ipv4 and not ipv6:
-        raise OptionError("Neither IPv4 nor IPv6 is chosen.")
-    if prefix is not None and interface is not None:
-        raise OptionError("Cannot specify both prefix and interface.")
-    if not interface:
-        if ipv4:
-            for _, _, _, _, sockaddr in Local.ipv4_list:
-                if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
-                    return str(sockaddr[0])
-        if ipv6:
-            for _, _, _, _, sockaddr in Local.ipv6_list:
-                if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
-                    return str(sockaddr[0])
-    else:
-        # Use the interface name specified to get the IP address
-        if ipv4:
-            for _, _, _, canonname, sockaddr in Local.ipv4_list:
-                if canonname == interface:
-                    return str(sockaddr[0])
-        if ipv6:
-            for _, _, _, canonname, sockaddr in Local.ipv6_list:
-                if canonname == interface:
-                    return str(sockaddr[0])
-    raise NotFoundError(f"No matching local IP address matching prefix={prefix}, interface={interface} found.")
-
-def get_all_local_ip(
-    ipv4: bool = True, ipv6: bool = False, prefix: str | None = None, interface: str | None = None
-) -> list[str]:
-    if not ipv4 and not ipv6:
-        raise OptionError("Neither IPv4 nor IPv6 is chosen.")
-    result: list[str] = []
-    if ipv4:
-        for _, _, _, _, sockaddr in Local.ipv4_list:
-            if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
-                result.append(str(sockaddr[0]))
+def get_local_ip(interface: str, ipv6: bool = False) -> str:
+    local_interfaces = interfaces()
+    if interface not in local_interfaces:
+        raise NotFoundError(f"Interface {interface} not found.")
+    family = AF_INET6 if ipv6 else AF_INET
+    address = ifaddresses(interface).setdefault(family, [{}])[0].get("addr", "")
     if ipv6:
-        for _, _, _, _, sockaddr in Local.ipv6_list:
-            if prefix is None or sockaddr[0].startswith(prefix):    # type: ignore
-                result.append(str(sockaddr[0]))
-    return result
+        # Remove the scope id if present
+        address = address.split("%")[0]
+    return address
+
+
+def get_all_local_ip(ipv6: bool = False) -> dict[str, str]:
+    ips = {}
+    for interface in interfaces():
+        if not ipv6:
+            family = AF_INET
+        else:
+            family = AF_INET6
+        address = ifaddresses(interface).setdefault(family, [{}])[0].get("addr", "")
+        if address:
+            if ipv6:
+                # Remove the scope id if present
+                address = address.split("%")[0]
+            ips[interface] = address
+    return ips
